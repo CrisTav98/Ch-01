7e9984ad4347f079e2f87bb98a5b7d5e208fc9dd
CrisTav98 <Cristian.tav98@gmail.com>
2025-01-28 18:15:28 -0500

    node.js

---
+++ Node.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+const ports = 3000;
+
+server.getMaxListeners('/hello', (req, res) => {
+    res.send('Hello World my name is cristian');
+});
+
+server.listen(ports, function () {
+    console.log('Listening on ' + ports);
+});
+
